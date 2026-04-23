06a04a8312bffd68e22d70cd322eafed2976f238
Jack625a <ktfut15@gmail.com>
2026-04-23 16:18:30 -0400

    Conexion Base de datos Online

--- basedatosOnline/src/main.py
+++ basedatosOnline/src/main.py
@@ -1,4 +1,5 @@
 import flet as ft
+import requests
 
 #conexion a la base de datos Airtable
 #token - idBasedatos
@@ -8,9 +9,59 @@ nombreTabla=""
 
 
 def main(page: ft.Page):
+
+    listaEstudiante=ft.ListView(
+        padding=20,
+        spacing=10,
+        expand=True
+    )
+
+    #CARGADO DE DATOS AL LISTVIEW
+    def cargarDatos():
+        url=f"https://api.airtable.com/v0/{idBaseDatos}/{nombreTabla}"
+        headers={
+            "Authorization":f"Bearer {tokenAirtable}"
+        }
+        respuesta=requests.get(url,headers=headers)
+
+        if respuesta.status_code==200:
+            data=respuesta.json()
+            #Limpiar el componente
+            listaEstudiante.controls.clear()
+
+            for registro in data["records"]:
+                campos=registro["fields"]
+                nombre=campos.get("Nombre","Sin Nombre")
+                carrera=campos.get("Carrera","Sin Carrera")
+                correo=campos.get("Correo","Sin Correo")
+                celular=campos.get("Celular","Sin Celular")
+                foto=campos.get("Foto","Sin Foto")
+
+                tarjeta=ft.Card(
+                    content=ft.Container(
+                        content=ft.Column(
+                                    [
+                                    ft.Image(src=foto,width=100,height=100),
+                                    ft.Text(nombre),
+                                    ft.Text(carrera) ,
+                                    ft.Text(celular),
+                                    ]
+                        )
+
+                                   
+                                                
+                                
+                    ),
+                    
+                    
+                )
+                listaEstudiante.controls.append(tarjeta)
+        else:
+            print("Error....")
     
+    cargarDatos()
     page.add(
-        
+        listaEstudiante 
     )
 
 
